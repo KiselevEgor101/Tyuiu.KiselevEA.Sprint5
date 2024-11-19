937ddd6352eba5a9d9d6d1eaee32c6568d9f7ef9
Tyuiu.KiselevEA@outlook.com <Tyuiu.KiselevEA@outlook.com>
2024-11-20 01:43:38 +0500

    123

--- Tyuiu.KiselevEA.Sprint5.Task1.V29.Lib/DataService.cs
+++ Tyuiu.KiselevEA.Sprint5.Task1.V29.Lib/DataService.cs
@@ -6,25 +6,33 @@ namespace Tyuiu.KiselevEA.Sprint5.Task1.V29.Lib
     {
         public string SaveToFileTextData(int startValue, int stopValue)
         {
+            string stry = string.Empty;
             string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask1.txt" });
+           
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
 
-            if (fileExists) File.Delete(path);
-
+            if (fileExists)
+            {
+                File.Delete(path);
+            }
+            int x;
             double y;
-            string strY;
-
-            for (int x = startValue; x <= stopValue; x++)
+            
+            for (x = startValue; x <= stopValue; x++)
             {
                 y = Math.Round((Math.Sin(x) / (x + 1.2)) + Math.Cos(x) * 7 * x - 2, 2);
-                strY = Convert.ToString(y);
-                if (x + 1.2 == 0) y = 0;
-
-                if (x != stopValue) File.AppendAllText(path, strY + Environment.NewLine);
-                else File.AppendAllText(path, strY);
+                stry = Convert.ToString(y);
+            }           
+            if (x != stopValue)
+            {
+                File.AppendAllText(path, stry + Environment.NewLine);
             }
-
+            else
+            {
+                File.AppendAllText(path, stry);
+            }
+            
             return path;
         }
     }
