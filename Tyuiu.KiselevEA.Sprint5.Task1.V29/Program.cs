50e99a19e0a0365e46234d8a1a9db467275fa3e1
Tyuiu.KiselevEA@outlook.com <Tyuiu.KiselevEA@outlook.com>
2024-11-20 01:20:27 +0500

    123

---
+++ Tyuiu.KiselevEA.Sprint5.Task1.V29/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.KiselevEA.Sprint5.Task1.V29.Lib;
+namespace Tyuiu.KiselevEA.Sprint5.Task1.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startValue = -5;
+            int stopValue = 5;
+
+            DataService ds = new DataService();
+
+            Console.WriteLine("Начальное значение диапазона = " + startValue);
+            Console.WriteLine("Конечное значение диапазона = " + stopValue);
+
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("# РЕЗУЛЬТАТ:                                                              #");
+            Console.WriteLine("###########################################################################");
+
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+
+            
+
+            
+        }
+    }
+}
