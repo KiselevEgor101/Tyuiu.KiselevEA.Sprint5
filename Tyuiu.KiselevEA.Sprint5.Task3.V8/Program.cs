4034af752c1cb5dc5e7736fbffbcdee3257bd744
Tyuiu.KiselevEA@outlook.com <Tyuiu.KiselevEA@outlook.com>
2024-11-23 13:50:04 +0500

    123

---
+++ Tyuiu.KiselevEA.Sprint5.Task3.V8/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.KiselevEA.Sprint5.Task3.V8.Lib;
+namespace Tyuiu.KiselevEA.Sprint5.Task3.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Киселев Е. А. | СМАРТб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Киселев Егор Алексеевич | СМАРТб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Вычислить выражение и вывести его в файл .csv                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 3;
+
+            DataService ds = new DataService();
+
+
+            Console.WriteLine("* МАССИВ:    *");
+
+           
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
