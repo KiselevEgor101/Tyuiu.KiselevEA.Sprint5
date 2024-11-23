4bd0448db2475118c0564620e1e9069e1f6fde22
Tyuiu.KiselevEA@outlook.com <Tyuiu.KiselevEA@outlook.com>
2024-11-23 22:11:42 +0500

    123

--- Tyuiu.KiselevEA.Sprint5.Task4.V3.Lib/DataService.cs
+++ Tyuiu.KiselevEA.Sprint5.Task4.V3.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.KiselevEA.Sprint5.Task4.V3.Lib
         public double LoadFromDataFile(string path)
         {
             string x = File.ReadAllText(path).Replace('.', ',');
-            double res = (((Math.Sin(Convert.ToDouble(x)) + 4) / Convert.ToDouble(x)) + (1.25 * Convert.ToDouble(x)));
+            double res = (((Math.Sin(Convert.ToDouble(x)) + 4) / Convert.ToDouble(x)) - (1.25 * Convert.ToDouble(x)));
             res = Math.Round(res, 3);
             return res;
         }
