38439f6e282c8649129ac32f6f0706f29caa2ed4
Tyuiu.KiselevEA@outlook.com <Tyuiu.KiselevEA@outlook.com>
2024-11-25 21:05:07 +0500

    123

---
+++ Tyuiu.KiselevEA.Sprint5.Task6.V20.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+namespace Tyuiu.KiselevEA.Sprint5.Task6.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.Combine(Path.GetTempPath(), "InPutDataFileTask6V20.txt");
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
